f65e865f06763efccc5bfa68b8b215187019f5fd
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-10 21:38:44 +0900

    Title: 사칙연산 함수, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240710/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void plus(int a, int b) {
+        System.out.println(a + " + " + b + " = " + (a+b));
+    }
+    public static void minus(int a, int b) {
+        System.out.println(a + " - " + b + " = " + (a-b));
+    }
+    public static void multiple(int a, int b) {
+        System.out.println(a + " * " + b + " = " + (a*b));
+    }
+    public static void divide(int a, int b) {
+        System.out.println(a + " / " + b + " = " + (a/b));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String s = sc.next();
+        int b = sc.nextInt();
+
+        if (s.equals("+")) {
+            plus(a, b);
+        }
+        else if (s.equals("-")) {
+            minus(a, b);
+        }
+        else if (s.equals("*")) {
+            multiple(a, b);
+        }
+        else if (s.equals("/")) {
+            divide(a, b);
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
