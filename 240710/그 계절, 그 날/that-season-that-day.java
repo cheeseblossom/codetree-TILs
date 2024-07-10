7e8a8b281ccd3f7193b19c3001874fd045652315
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-10 22:49:15 +0900

    Title: 그 계절, 그 날, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240710/그 계절, 그 날/that-season-that-day.java
+++ 240710/그 계절, 그 날/that-season-that-day.java
@@ -2,53 +2,60 @@ import java.util.Scanner;
 
 public class Main {
     public static boolean isYoonYear(int n) {
-        if ((n % 4 == 0 && n % 100 == 0) || n % 400 == 0) {
-            return true;
-        }
-        if (n % 4 == 0 && n % 100 == 0) {
-            return false;
-        }
         if (n % 4 == 0) {
             return true;
         }
-        
+        else if (n % 4 == 0 && n % 100 == 0) {
+            return false;
+        }
+        else if (n % 4 == 0 && n % 100 == 0 && n % 400 == 0) {
+            return true;
+        }
         return false;
     }
-    public static void checkDate(int y, int m, int d) {
+    public static boolean isValidDate(int y, int m, int d) {
         if (m == 2) {
             if (isYoonYear(y)) {
                 if (d > 29) {
-                    System.out.println("-1");
+                    return false;
                 }
             }
             else {
                 if (d > 28) {
-                    System.out.println("-1");
+                    return false;
                 }
             }
         }
         else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
             if (d > 31) {
-                System.out.println("-1");
+                return false;
             }
         }
         else  {
             if (d > 30) {
-                System.out.println("-1");
+                return false;
             }
         }
+        return true;
+    }
 
-        if (3 <= m && m <= 5) {
-            System.out.println("Spring");
-        }
-        else if (6 <= m && m <= 8) {
-            System.out.println("Summer");
-        }
-        else if (9 <= m && m <= 11) {
-            System.out.println("Fall");
+    public static void checkDate(int y, int m, int d) {
+        if (isValidDate(y, m, d)) {
+            if (3 <= m && m <= 5) {
+                System.out.println("Spring");
+            }
+            else if (6 <= m && m <= 8) {
+                System.out.println("Summer");
+            }
+            else if (9 <= m && m <= 11) {
+                System.out.println("Fall");
+            }
+            else {
+                System.out.println("Winter");
+            }
         }
         else {
-            System.out.println("Winter");
+            System.out.println("-1");
         }
     }
 
