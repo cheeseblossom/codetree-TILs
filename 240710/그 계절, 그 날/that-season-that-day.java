a51ffe9c4b62d93a33df79fd8f3068d92d5e9400
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-10 22:36:24 +0900

    Title: 그 계절, 그 날, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240710/그 계절, 그 날/that-season-that-day.java
+++ 240710/그 계절, 그 날/that-season-that-day.java
@@ -1,37 +1,64 @@
 import java.util.Scanner;
-import java.text.SimpleDateFormat;
 
 public class Main {
-    public static void checkDate(int a, int b, int c) {
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-            String strB = b < 10 ? "0" + String.valueOf(b) : String.valueOf(b);
-            String strC = c < 10 ? "0" + String.valueOf(c) : String.valueOf(c);
-            sdf.setLenient(false);
-            sdf.parse(String.valueOf(a) + strB + strC);
-            if (b >= 3 && b <= 5) {
-                System.out.println("Spring");
-            }
-            else if (b >= 6 && b <= 8) {
-                System.out.println("Summer");
-            }
-            else if (b >= 9 && b <= 11) {
-                System.out.println("Fall");
+    public static boolean isYoonYear(int n) {
+        if (n % 4 == 0) {
+            return true;
+        }
+        if (n % 4 == 0 && n % 100 == 0) {
+            return false;
+        }
+        if (n % 4 == 0 && n % 100 == 0 && n % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
+    public static void checkDate(int y, int m, int d) {
+        if (m == 2) {
+            if (isYoonYear(y)) {
+                if (d > 29) {
+                    System.out.println("-1");
+                }
             }
             else {
-                System.out.println("Winter");
+                if (d > 28) {
+                    System.out.println("-1");
+                }
             }
         }
-        catch (Exception e) {
+        else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+            if (d > 31) {
+                System.out.println("-1");
+            }
+        }
+        else if (m == 4 || m == 6 || m == 9 || m == 11) {
+            if (d > 30) {
+                System.out.println("-1");
+            }
+        }
+        else {
             System.out.println("-1");
+        }
+
+        if (3 <= m && m <= 5) {
+            System.out.println("Spring");
+        }
+        else if (6 <= m && m <= 8) {
+            System.out.println("Summer");
+        }
+        else if (9 <= m && m <= 11) {
+            System.out.println("Fall");
+        }
+        else {
+            System.out.println("Winter");
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        checkDate(a, b, c);
+        int year = sc.nextInt();
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        checkDate(year, month, day);
     }
 }
