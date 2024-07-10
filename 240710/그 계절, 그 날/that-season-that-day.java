c0ce3a1bab43a184269291a1ed930ae30cfa6446
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-10 22:52:57 +0900

    Title: 그 계절, 그 날, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240710/그 계절, 그 날/that-season-that-day.java
+++ 240710/그 계절, 그 날/that-season-that-day.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static boolean isYoonYear(int n) {
-        if (n % 4 == 0) {
+        if (n % 4 == 0 && n % 100 == 0 && n % 400 == 0) {
             return true;
         }
         else if (n % 4 == 0 && n % 100 == 0) {
             return false;
         }
-        else if (n % 4 == 0 && n % 100 == 0 && n % 400 == 0) {
+        else if (n % 4 == 0) {
             return true;
         }
         return false;
