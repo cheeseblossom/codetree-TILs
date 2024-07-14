3490ef50e20b21975dfd5e54219211d8abb889c3
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-14 20:54:33 +0900

    Title: 짝수만 2로 나누기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240714/짝수만 2로 나누기/divide-even-numbers-by-2.java
+++ 240714/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int N = 50;
+    public static int[] arr = new int[N];
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        printDivideEven(sc);
-    }
-
-    public static void printDivideEven(Scanner sc) {
         int a = sc.nextInt();
         for (int i=0; i<a; i++) {
-            int nextItem = sc.nextInt();
-            if (nextItem % 2 == 0) {
-                nextItem /= 2;
-            }
-            System.out.print(nextItem);
-            if (i != a-1) {
-                System.out.print(" ");
+            arr[i] = sc.nextInt();
+        }
+        divideEven(a);
+        for (int i=0; i<a; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void divideEven(int a) {
+        for (int i=0; i<a; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i] /= 2;
             }
         }
     }
