e310191eb897d8d089b7044379e4b97114dee9d4
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-15 20:43:23 +0900

    Title: 재귀함수의 꽃, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240715/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        printNumber(a);
+    }
+
+    public static void printNumber(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.print(n + " ");
+        printNumber(n-1);
+        System.out.print(n + " ");
+    }
+}
