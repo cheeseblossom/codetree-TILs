8386a5d30e11d23424a56a04bf6036b111839590
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-16 22:12:59 +0900

    Title: 중앙값 계산 2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240716/중앙값 계산 2/get-median-2.java
+++ 240716/중앙값 계산 2/get-median-2.java
@@ -8,7 +8,7 @@ public class Main {
         int[] arr = new int[a];
         for (int i=0; i<a; i++) {
             arr[i] = sc.nextInt();
-            if (arr[i] % 2 != 0) {
+            if ((i+1) % 2 != 0) {
                 Arrays.sort(arr, 0, i);
                 System.out.print(arr[i/2] + " ");
             }
