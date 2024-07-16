d65d2fc8da7217548ad20cff9d85cedc43736829
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-16 17:11:38 +0900

    Title: 이상한 수열, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240716/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print(printNum(a));
+    }
+
+    public static int printNum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return printNum(n/3) + printNum(n-1);
+    }
+}
