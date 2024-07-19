0d085dcf834304e9222e576299f0c8c9e8516004
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-19 16:57:28 +0900

    Title: DateTime to DateTime, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240719/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d = 11, h = 11, m = 11;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int result = 0;
+
+        if (a<= 11 && b <= 11 && c <= 11) {
+            System.out.print(-1);
+        }
+        else {
+            while (true) {
+                if (d == a && h == b && m == c) {
+                    break;
+                }
+                m++;
+                result++;
+                if (m == 60) {
+                    m = 0;
+                    h++;
+                }
+                if (h == 24) {
+                    h = 0;
+                    d++;
+                }
+            }
+            System.out.print(result);
+        }
+    }
+}
