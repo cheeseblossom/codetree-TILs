c315f3e142ea0230f0437da7d5544c5fc4e3ba00
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-19 22:21:03 +0900

    Title: 그 요일은, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240719/그 요일은/the-day-of-the-day.java
+++ 240719/그 요일은/the-day-of-the-day.java
@@ -3,36 +3,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] days = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        String[] dates = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-        String day = sc.next();
+        String date = sc.next();
 
-        int index = 0;
-        for (int i=0; i<days.length; i++) {
-            if (days[i].equals(day)) {
-                index = i;
-                break;
-            }
-        }
-
-        d1 += index;
-
-        int[] dates = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int result = 0;
-        while (true) {
-            if (m1 == m2 && d1 >= d2) {
-                break;
-            }
-            d1 += 7;
-            result++;
-            if (dates[m1] < d1) {
-                d1 = 1;
-                m1++;
+        int diff = numOfDays(m2, d2) - numOfDays(m1, d1) + 1;
+        int q = diff / 7;
+        int r = diff % 7;
+        int result = q;
+        for (int i=0; i<r; i++) {
+            if (dates[i].equals(date)) {
+                result++;
             }
         }
         System.out.print(result);
+    }
+
+    public static int numOfDays(int m, int d) {
+        int[] days = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+
+        // 1월부터 m-1월까지는 전부 계산
+        for (int i=1; i<m; i++) {
+            totalDays += days[i];
+        }
+        // m월은 d까지만
+        totalDays += d;
+        return totalDays;
     }
 }
