3b397d8dc82bd45842e4ac06d3fd83c5b1220396
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-19 21:52:27 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240719/요일 맞추기/guess-day-of-week.java
+++ 240719/요일 맞추기/guess-day-of-week.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         String[] day = new String[]{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
         int m1 = sc.nextInt();
@@ -11,50 +10,24 @@ public class Main {
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
 
-        boolean isReverse = false;
-        if (m1 > m2) {
-            isReverse = true;
-            int temp = m1;
-            m1 = m2;
-            m2 = temp;
-            temp = d1;
-            d1 = d2;
-            d2 = d1;
-        }
-        else if (m1 == m2) {
-            if (d1 > d2) {
-                isReverse = true;
-                int temp = d1;
-                d1 = d2;
-                d2 = temp;
-            }
-        }
-        int gap = 0;
-        while (true) {
-            if (m1 == m2 && d1 == d2) {
-                break;
-            }
-            d1++;
-            gap++;
-            if (arr[m1] < d1) {
-                d1 = 1;
-                m1++;
-            }
-        }
+        int diff = numOfDays(m2, d2) - numOfDays(m1, d1);
 
-        if (isReverse) {
-            gap %= 7;
-            if (gap < 2) {
-                gap = 7 + gap;
-            }
-            System.out.print(day[8-gap]);
+        while (diff < 0) {
+            diff += 7;
         }
-        else {
-            gap %= 7;
-            if (gap == 6) {
-                gap = -1;
-            }
-            System.out.print(day[gap+1]);
+        System.out.print(day[diff % 7]);
+    }
+
+    public static int numOfDays(int m, int d) {
+        int[] arr = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+
+        // 1월부터 m-1월까지는 전부 계산
+        for (int i=1; i<m; i++) {
+            totalDays += days[i];
         }
+        // m월은 d까지만
+        totalDays += d;
+        return totalDays;
     }
 }
