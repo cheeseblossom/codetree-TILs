5b2259b984bc70c9cb3d6da3b434cce31590c4cd
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2024-07-19 21:00:19 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240719/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] day = new String[]{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        boolean isReverse = false;
+        int gap = 0;
+
+        if (m1 > m2) {
+            isReverse = true;
+            int temp = m1;
+            m1 = m2;
+            m2 = temp;
+        }
+        else {
+            if (d1 > d2) {
+                isReverse = true;
+                int temp = d1;
+                d1 = d2;
+                d2 = temp;
+            }
+        }
+
+        while (true) {
+            if (m1 == m2 && d1 == d2) {
+                break;
+            }
+            d1++;
+            gap++;
+            if (d1 == arr[m1]) {
+                d1 = 1;
+                m1++;
+            }
+        }
+
+        if (isReverse) {
+            gap = gap * -1;
+        }
+
+        int result = gap % 7;
+        System.out.print(day[1+result]);
+    }
+}
